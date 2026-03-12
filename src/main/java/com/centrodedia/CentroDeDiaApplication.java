ae3291d1b8e9c158279c8824613e622626651d5b
Miguel Montero <miganml@outlook.com>
2026-03-12 11:05:19 +0100

    feat: project base created with start.spring.io (Spring initializer)

---
+++ src/main/java/com/centrodedia/CentroDeDiaApplication.java
@@ -0,0 +1,13 @@
+package com.centrodedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroDeDiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentroDeDiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/centrodedia/CentroDeDiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centrodedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroDeDiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
